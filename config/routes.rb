768689ca174fc3cfc7e59a1f5a53e9f2b076de20
steve-alex <steve.alex@hotmail.co.uk>
2019-09-27 15:31:50 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  get '/new' => 'sessions#new', as: "new_session"
+  post '/login' => 'sessions#create', as: "create_session"
+  post '/logout' => 'sessions#destroy', as: "destroy_session"
 end
